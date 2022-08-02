7d0ad494c55cccd313119d4247a5022bbe07f8cd
Adam <hadrbabaa@gmail.com>
2022-08-02 10:40:19 +0200

    .

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,8 @@ import sveltePreprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-netlify';
 
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
+
+	
 
 	kit: {
 		// By default, `npm run build` will create a standard Node app.
@@ -19,6 +18,9 @@ const config = {
 			pages: ['*'],
 		  },		    
 	},
+		// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: sveltePreprocess(),
 };
 
 export default config;
