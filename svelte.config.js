a75f726489f8897fafb33a2eeb5f18a47084a967
Adam <hadrbabaa@gmail.com>
2022-08-02 10:04:26 +0200

    adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,20 @@
-import adapter from '@sveltejs/adapter-netlify';
+import sveltePreprocess from 'svelte-preprocess';
+import node from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: sveltePreprocess(),
+
 	kit: {
-		adapter: adapter(),
-		//prerender: {
-	//		default: true
-	//	}
+		// By default, `npm run build` will create a standard Node app.
+		// You can create optimized builds for different platforms by
+		// specifying a different adapter
+		adapter: node(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
 	}
 };
 
