2f639926250a26fd684db1e3113460e46e45afd0
Adam <hadrbabaa@gmail.com>
2022-08-02 10:16:43 +0200

    .

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import sveltePreprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
@@ -11,10 +10,7 @@ const config = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: adapter({
-			edge: false,
-			split: false
-		}),
+		adapter: adapter()
 	}
 };
 
